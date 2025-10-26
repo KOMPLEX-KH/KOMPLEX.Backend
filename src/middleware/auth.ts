9998a4fa0a15d38c79df9f79c98939d4f83bf794
Chanraksa Ory <OCraksa@gmail.com>
2025-10-26 11:22:59 +0700

    secure route for admins

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { AuthenticatedRequest } from "../types/request.js";
 import admin from "../config/firebase/admin.js";
 import { users } from "../db/schema.js";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { db } from "../db/index.js";
 
 export const verifyFirebaseToken = async (
@@ -26,6 +26,36 @@ export const verifyFirebaseToken = async (
 
     if (!user) {
       return res.status(401).json({ message: "User not found" });
+    }
+
+    req.user = { userId: user.id };
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token", error });
+  }
+};
+
+export const verifyFirebaseTokenAdmin = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Missing token" });
+  }
+
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+
+    // exchange uid for user id
+    const [user] = await db
+      .select()
+      .from(users)
+      .where(and(eq(users.uid, decoded.uid), eq(users.isAdmin, true)));
+
+    if (!user || !user.isAdmin) {
+      return res.status(401).json({ message: "Invalid token" });
     }
 
     req.user = { userId: user.id };
--- src/server.ts
+++ src/server.ts
@@ -60,7 +60,7 @@ app.use(
   })
 );
 app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 
 app.use(globalRateLimiter);
 app.get("/ping", async (req, res) => {
