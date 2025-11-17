cd02215b8ad542efbad08852902a844cf2c2e98c
Chanraksa Ory <OCraksa@gmail.com>
2025-11-17 14:44:18 +0700

    changed topic history

--- src/db/models/user_ai_topic_history.ts
+++ src/db/models/user_ai_topic_history.ts
@@ -1,12 +1,12 @@
 import { text, pgTable, serial, integer, timestamp } from "drizzle-orm/pg-core";
-import { subjects } from "./subjects.js";
 import { users } from "./users.js";
 import { topics } from "./topics.js";
 
-export const aiTopics = pgTable("ai_topic", {
+export const userAiTopicHistories = pgTable("user_ai_topic_histories", {
 	id: serial("id").primaryKey(),
 	userId: integer("user_id").references(() => users.id),
 	topicId: integer("topic_id").references(() => topics.id),
+	prompt: text("prompt"),
 	aiResult: text("ai_result"),
 	createdAt: timestamp("created_at").defaultNow(),
 	updatedAt: timestamp("updated_at").defaultNow(),
