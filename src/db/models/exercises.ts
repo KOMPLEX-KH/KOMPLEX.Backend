53dacc15bb2658474989ed97dc7a206ba907673b
Chanraksa Ory <OCraksa@gmail.com>
2025-12-01 14:57:00 +0700

    added back user id

--- src/db/models/exercises.ts
+++ src/db/models/exercises.ts
@@ -6,12 +6,14 @@ import {
   timestamp,
   serial,
 } from "drizzle-orm/pg-core";
+import { users } from "./users.js";
 
 export const exercises = pgTable("exercises", {
   id: serial("id").primaryKey(),
   videoId: integer("video_id"),
   duration: integer("duration"),
   title: text("title"),
+  userId: integer("user_id").references(() => users.id),
   description: text("description"),
   subject: text("subject"),
   grade: varchar("grade"),
